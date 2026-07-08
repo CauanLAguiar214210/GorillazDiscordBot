5cc49bccbf4d518d83bd359f679558d31503d7fb
CauanLAguiar214210 <dev.cauanaguiar@gmail.com>
2026-07-08 16:46:12 -0300

    init refactor for aws

--- Configuration/BotOptions.cs
+++
@@ -1,7 +0,0 @@
-namespace GorillazDiscordBot.Configuration;
-
-public class BotOptions
-{
-    public string DiscordToken { get; set; } = string.Empty;
-    public string CommandPrefix { get; set; } = "macaco ";
-}
--- Services/Interfaces/IGifUrlService.cs
+++
@@ -1,6 +0,0 @@
-namespace GorillazDiscordBot.Services.Interfaces;
-
-public interface IGifUrlService
-{
-    Task<string> GetDirectUrlAsync(string url);
-}
