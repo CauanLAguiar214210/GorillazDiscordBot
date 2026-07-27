31a0fcbfa5601294574c6e5f2f51fee656aed0f1
CauanLAguiar214210 <dev.cauanaguiar@gmail.com>
2026-07-27 11:59:50 -0300

    implementado welcome

---
+++ GorillazDiscordBot.Domain/Entity/GuildWelcomeSettings.cs
@@ -0,0 +1,12 @@
+namespace GorillazDiscordBot.Entity;
+
+public class GuildWelcomeSettings
+{
+    public ulong GuildId { get; set; }
+    public ulong? WelcomeChannelId { get; set; }
+    public ulong? GoodbyeChannelId { get; set; }
+    public string WelcomeMessage { get; set; } = "Bem-vindo(a) {user} ao **{server}**! 🎉\nVocê é o membro nº **{count}**!";
+    public string GoodbyeMessage { get; set; } = "Tchau {user}! 😢\nVolte sempre ao **{server}**!";
+    public bool WelcomeEnabled { get; set; }
+    public bool GoodbyeEnabled { get; set; }
+}
