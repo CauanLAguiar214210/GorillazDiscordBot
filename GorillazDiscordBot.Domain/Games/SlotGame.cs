0d58ee93934e0c4f894a4bbd590fada30cb1a5ea
CauanLAguiar214210 <dev.cauanaguiar@gmail.com>
2026-08-27 00:55:36 -0300

    Fet - Modulo Cassino

---
+++ GorillazDiscordBot.Domain/Games/SlotGame.cs
@@ -0,0 +1,40 @@
+namespace GorillazDiscordBot.Domain.Games;
+
+/// <summary>Caça-níqueis com tema da selva (macacos, bananas, etc.).</summary>
+public sealed class SlotGame
+{
+    public static readonly IReadOnlyList<string> Symbols = new[]
+    {
+        "🐵", // macaco
+        "🍌", // banana
+        "🦍", // gorila
+        "🌴", // palmeira
+        "🥥"  // coco
+    };
+
+    public IReadOnlyList<string> Reels { get; }
+
+    public SlotGame()
+    {
+        Reels = new[] { Draw(), Draw(), Draw() };
+    }
+
+    private static string Draw() => Symbols[Random.Shared.Next(Symbols.Count)];
+
+    /// <summary>Multiplicador do valor total devolvido ao jogador (inclui a aposta). 0 = perda.</summary>
+    public int Multiplier
+    {
+        get
+        {
+            if (Reels[0] == Reels[1] && Reels[1] == Reels[2])
+                return 5;
+
+            if (Reels[0] == Reels[1] || Reels[1] == Reels[2] || Reels[0] == Reels[2])
+                return 2;
+
+            return 0;
+        }
+    }
+
+    public bool IsWin => Multiplier > 0;
+}
